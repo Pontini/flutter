7b8d47c9a264cae6b90f9698bb1e7aec2e9bc695
ewertonpontini <ewerton.pontini@gmail.com>
2020-01-16 08:33:18 -0300

    add products

--- shop/lib/screens/home_screen.dart
+++ shop/lib/screens/home_screen.dart
@@ -1,20 +1,31 @@
-
 import 'package:flutter/material.dart';
 import 'package:shop/tabs/home_tab.dart';
+import 'package:shop/tabs/products_tab.dart';
+import 'package:shop/widgets/custom_drawer.dart';
 
 class HomeScreen extends StatelessWidget {
-
-
-
+  final _pageController = PageController();
 
   @override
   Widget build(BuildContext context) {
     return PageView(
+      controller: _pageController,
       physics: NeverScrollableScrollPhysics(),
       children: <Widget>[
-      HomeTab()
+        Scaffold(
+          body: HomeTab(),
+          drawer: CustomDrawer(_pageController),
+        ),
+        Scaffold(
+          appBar: AppBar(
+            title: Text("Produtos"),
+            centerTitle: true,
+          ),
+          drawer: CustomDrawer(_pageController),
+          body: ProductsTab(),
+        ),
+
       ],
-      
     );
   }
 }
--- shop/lib/tabs/home_tab.dart
+++ shop/lib/tabs/home_tab.dart
@@ -1,7 +1,6 @@
-
+import 'package:flutter/material.dart';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:transparent_image/transparent_image.dart';
 
@@ -9,77 +8,74 @@ class HomeTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    Widget _buildBodyBack()=>Container(
+    Widget _buildBodyBack() => Container(
       decoration: BoxDecoration(
-        gradient: LinearGradient(colors:[
-          Color.fromARGB(255, 211, 118, 130),
-          Color.fromARGB(255, 211, 253, 168)
-
-        ],
-        begin: Alignment.topLeft,
-        end:  Alignment.bottomLeft
-        
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 211, 118, 130),
+            Color.fromARGB(255, 253, 181, 168)
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight
         )
       ),
-
     );
+
+//Quando vc quer colocar algo em cima da outra, vc usa stack. Nesse caso, tudo que está abaixo vai pra cima.
     return Stack(
       children: <Widget>[
         _buildBodyBack(),
         CustomScrollView(
           slivers: <Widget>[
-            SliverAppBar(
+            SliverAppBar( 
               floating: true,
-          snap: true,
-          backgroundColor: Colors.transparent,
-          elevation: 0.0,
-          flexibleSpace: FlexibleSpaceBar(
-            //Otimização
-            title: const Text("Novidades"),
-            centerTitle: true,
-          ),
-        )],),
-        FutureBuilder<QuerySnapshot>(
-          future: Firestore.instance.collection("home").orderBy("pos").getDocuments(),
-          builder: (context, snapshot){
-            if(!snapshot.hasData){
-              return SliverToBoxAdapter(child: Container(height: 200,alignment: Alignment.center,child: CircularProgressIndicator(
-                valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-              ),),);
-
-            }
-            else{
-              return SliverStaggeredGrid.count(
-                crossAxisCount: 2,
-                mainAxisSpacing: 1.0,
-                crossAxisSpacing: 1.0,
-                staggeredTiles: snapshot.data.documents.map(
-                  (doc){
-                    return StaggeredTile.count(doc.data["x"], doc.data["y"]);
-                  }
-                ).toList(),
-                children: snapshot.data.documents.map(
-                    (doc){
-
+              snap: true, //Fazer efeito de subir e descer.
+              backgroundColor: Colors.transparent,
+              elevation: 0.0,
+              flexibleSpace: FlexibleSpaceBar(
+                title: const Text("Novidades"),
+                centerTitle: true,
+              ),
+            ),
+            FutureBuilder<QuerySnapshot>(
+              future: Firestore.instance
+                .collection("home").orderBy("pos").getDocuments(),
+              builder: (context, snapshot){
+                if(!snapshot.hasData)
+                  return SliverToBoxAdapter(
+                    child: Container(
+                      height: 200.0,
+                      alignment: Alignment.center,
+                      child: CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                      ),
+                    ),
+                  );
+                else
+                  return SliverStaggeredGrid.count(
+                      crossAxisCount: 2,
+                    mainAxisSpacing: 1.0,
+                    crossAxisSpacing: 1.0,
+                    staggeredTiles: snapshot.data.documents.map(
+                      (doc){
+                        return StaggeredTile.count(doc.data["x"], doc.data["y"]);
+                      }
+                    ).toList(),
+                    children: snapshot.data.documents.map(
+                      (doc){
                         return FadeInImage.memoryNetwork(
-                          placeholder: kTransparentImage, image: doc.data["image"],
-                          fit: BoxFit.cover,
+                            placeholder: kTransparentImage,
+                            image: doc.data["image"],
+                            fit: BoxFit.cover,
                         );
-                    }
-
-                ).toList(),
-
-              );
-            }
-
-
-          },
-
+                      }
+                    ).toList(),
+                  );
+              },
+            )
+          ],
         )
-      
       ],
-      
-
     );
   }
 }
