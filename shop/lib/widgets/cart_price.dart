15d02236e406efe8e91afad039cebbd6e0114d95
ewertonpontini <ewerton.pontini@gmail.com>
2020-01-27 00:27:52 -0300

    no message

---
+++ shop/lib/widgets/cart_price.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class CartPrice extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.symmetric(horizontal: 8.0,vertical: 4.0),
+    );
+  }
+}
